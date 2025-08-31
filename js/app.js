d9b6164cf1ea0dc75d91faa5eacdce020e4bd087
rubendml <rubendml@gmail.com>
2025-08-30 19:45:03 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -206,8 +206,7 @@ function renderCatalogo() {
         <p class="text-sm text-blue-800"><strong>Rareza:</strong> ${item.rareza}</p>
         <p class="text-sm text-blue-300 mt-2">${item.observaciones}</p>
 
-        <!-- Botones de editar/eliminar -->
-        <div class="mt-4 flex justify-end gap-2">
+        <!-- Botones de editar/eliminar -->        <div class="mt-4 flex justify-end gap-2">
           <button 
             onclick="editarDenominacionDesdeCatalogo('${item.id}')" 
             class="text-yellow-400 hover:text-yellow-300 text-sm font-medium transition"
